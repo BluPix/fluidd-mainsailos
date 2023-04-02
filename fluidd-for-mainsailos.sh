2f8922902058d402e0b8d16131e455419570363b
BluPix <nemec.jiri@gmail.com>
2023-04-02 10:51:13 +0200

    Added some statements about what the script does

--- fluidd-for-mainsailos.sh
+++ fluidd-for-mainsailos.sh
@@ -3,6 +3,7 @@
 cd ~
 LATEST_VERSION=$(curl -L -s -H 'Accept: application/json' https://github.com/fluidd-core/fluidd/releases/latest | sed -e 's/.*"tag_name":"\([^"]*\)".*/\1/')
 ARTIFACT_URL="https://github.com/fluidd-core/fluidd/releases/download/$LATEST_VERSION/fluidd.zip"
+echo Downloading fluidd $LATEST_VERSION
 echo $ARTIFACT_URL
 curl $ARTIFACT_URL -L --output /tmp/fluidd.zip
 unzip -qo /tmp/fluidd.zip -d ~/fluidd
@@ -12,6 +13,7 @@ ln -sf ~/fluidd ~/mainsail
  
 #modfify moonraker.conf
 if [[ $(grep -L fluidd-core/fluidd $HOME/printer_data/config/moonraker.conf) ]]; then
+ Updating $HOME/printer_data/config/moonraker.conf
  cp $HOME/printer_data/config/moonraker.conf $HOME/printer_data/config/moonraker.conf.backup
  awk '/^\[update_manager/{
   if($0~/update_manager mainsail]/){
@@ -31,5 +33,7 @@ path: ~/fluidd
 EOF
  
 fi
- 
+
+echo Restaring moonraker
 systemctl restart moonraker.service
+echo Finished
